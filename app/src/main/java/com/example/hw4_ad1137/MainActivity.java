6195063d47767d3c34653300830d19e9e5555ae0
AndreaDumalagan <43146256+AndreaDumalagan@users.noreply.github.com>
2019-11-24 16:20:49 -0500

    Is this a new homework assignment?

---
+++ app/src/main/java/com/example/hw4_ad1137/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.hw4_ad1137;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PaintView paintView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        paintView =  findViewById(R.id.paintView);
+
+        DisplayMetrics metrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        paintView.init(metrics);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.normal:
+                return true;
+            case R.id.emboss:
+                return true;
+            case R.id.blur:
+                return true;
+            case R.id.clear:
+                paintView.clear();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*class DrawingView extends SurfaceView{
+
+        private final SurfaceHolder surfaceHolder;
+        private final Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+        public DrawingView(Context context){
+            super(context);
+            surfaceHolder = getHolder();
+            paint.setColor(Color.YELLOW);
+            paint.setStyle(Paint.Style.FILL);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event){
+            if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                if (surfaceHolder.getSurface().isValid()) {
+                    Canvas canvas = surfaceHolder.lockCanvas();
+                    canvas.drawColor(Color.WHITE);
+                    canvas.drawCircle(event.getX(), event.getY(), 50, paint);
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+
+                    Toast.makeText(getBaseContext(), event.getX() + ", " + event.getY(), Toast.LENGTH_SHORT).show();
+                }
+            }
+            return false;
+        }
+    }*/
+}
